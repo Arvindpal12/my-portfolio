ba5ba1532aac3b59b95a56b7ccbb4099e5ddc1e1
arvind <Arvindpal6706@gmail.com>
2025-04-23 22:54:48 +0530

    secound commit

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import aboutImg from "../../assets/avtar6.webp"
 
-const ProjectCard = ({ title, main }) => {
-  const handleClick = () => {
-    window.open('https://github.com/Arvindpal12/Restaurant-project.git')
+const ProjectCard = ({ title, main, onClick }) => {
+  // const handleClick = () => {
+  //   window.open('https://github.com/Arvindpal12/Restaurant-project.git')
 
-  }
+  // }
   return (
     <div className='p-3 md:p-6 flex flex-col w-80 bg-[#0c0e19] shadow-xl shadow-slate-900 rounded-2xl transition-transform duration-200 ease-in-out hover:scale-125'>
       <img className="p-4" src={aboutImg} alt="" />
@@ -15,7 +15,7 @@ const ProjectCard = ({ title, main }) => {
       <p className='px-4 text-sm md:text-md leading-tight py-2'>{main}</p>
       <div className='mt-2 p-2 md:p-4 flex gap-2 md:gap-4'>
         <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#455697]'>Demo</button>
-        <button onClick={handleClick} className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#455697]'>Source code</button>
+        <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#455697]'onClick={onClick}>Source code</button>
 
       </div>
     </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,16 +2,29 @@ import React from 'react'
 import ProjectCard from './ProjectCard'
 
 const Projects = () => {
+  const handleClick1=()=>{
+    window.open('https://github.com/Arvindpal12/Restaurant-project.git')
+
+  };
+  const handleClick2=()=>{
+    window.open('https://github.com/Arvindpal12/project-currency-converter.git')
+
+  };
+  const handleClick3=()=>{
+    window.open('https://github.com/Arvindpal12/AI-based-Project.git')
+
+  }
+
   return (
     <div id='projects' className='p-10 md:p-24 text-white'>
         <h1 className='text-2xl md:text-4xl text-white font-bold'> Projects</h1>
         <div className='py-12 px-8 flex flex-wrap gap-5 justify-center'>
             <ProjectCard title="Resturant Website"
-            main="this is a rasturent website create in bootstrap and use some features and using basic css and html"/>
+            main="this is a rasturent website create in bootstrap and use some features and using basic css and html" onClick={handleClick1}/>
             <ProjectCard title="Currency Converter"
-            main="this is a Currency Convertor create in js and use some features and using  css and html"/>
+            main="this is a Currency Convertor create in js and use some features and using  css and html" onClick={handleClick2}/>
             <ProjectCard title="AI Assistent"
-            main="this is a AI Assistent create in js and use some features and using  css and html"/>
+            main="this is a AI Assistent create in js and use some features and using  css and html"onClick={handleClick3}/>
 
 
 
