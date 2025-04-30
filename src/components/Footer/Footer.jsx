89301af2c422c01536e83787d48f8da2ee7f7d5e
Arvind <palshahab1290@gmail.com>
2025-04-30 18:52:38 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     return (
         <div id='Footer' className='flex justify-around bg-[#0e1946] text-white p-10 md:p-12 items-center'>
             <div>
-                <h1 className='text-2xl md:text-6xl font-bold'>Contect</h1>
+                <h1 className='text-2xl md:text-6xl font-bold'>Contact</h1>
                 <h3 className='text-sm md:text-2xl font-normal'>Feel free to reach out</h3>
 
             </div>
