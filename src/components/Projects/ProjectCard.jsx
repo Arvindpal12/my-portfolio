c87aa9dd9b91e54e6e015b05b9cfd4525f84c349
Arvind <palshahab1290@gmail.com>
2025-09-10 18:17:22 +0530

    demo-btn changes

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import aboutImg from "../../assets/avtar6.webp"
 
-const ProjectCard = ({ title, main, onClick }) => {
-  // const handleClick = () => {
-  //   window.open('https://github.com/Arvindpal12/Restaurant-project.git')
-
-  // }
+const ProjectCard = ({ title, main, onDemoClick, onSourceClick }) => {
   return (
     <div className='p-3 md:p-6 flex flex-col w-80 bg-[#0c0e19] shadow-xl shadow-slate-900 rounded-2xl transition-transform duration-200 ease-in-out hover:scale-125'>
       <img className="p-4" src={aboutImg} alt="" />
@@ -14,8 +10,8 @@ const ProjectCard = ({ title, main, onClick }) => {
       </h3>
       <p className='px-4 text-sm md:text-md leading-tight py-2'>{main}</p>
       <div className='mt-2 p-2 md:p-4 flex gap-2 md:gap-4'>
-        <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#455697]'>Demo</button>
-        <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#455697]'onClick={onClick}>Source code</button>
+        <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#455697]' onClick={onDemoClick}>Demo</button>
+        <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#455697]' onClick={onSourceClick}>Source code</button>
 
       </div>
     </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,29 +2,38 @@ import React from 'react'
 import ProjectCard from './ProjectCard'
 
 const Projects = () => {
-  const handleClick1=()=>{
+  const handleDemo1 = () => {
+    window.open('https://github.com/Arvindpal12/Restaurant-project.git') // Placeholder for demo
+  };
+  const handleSource1 = () => {
     window.open('https://github.com/Arvindpal12/Restaurant-project.git')
-
   };
-  const handleClick2=()=>{
+
+  const handleDemo2 = () => {
+    window.open('https://currencyap.netlify.app/')
+  };
+  const handleSource2 = () => {
     window.open('https://github.com/Arvindpal12/project-currency-converter.git')
-
   };
-  const handleClick3=()=>{
-    window.open('https://github.com/Arvindpal12/AI-based-Project.git')
 
-  }
+  const handleDemo3 = () => {
+    window.open('https://ai-ap-projects.netlify.app/')
+  };
+  const handleSource3 = () => {
+    window.open('https://github.com/Arvindpal12/AI-based-Project.git')
+  };
 
   return (
     <div id='projects' className='p-10 md:p-24 text-white'>
         <h1 className='text-2xl md:text-4xl text-white font-bold'> Projects</h1>
         <div className='py-12 px-8 flex flex-wrap gap-5 justify-center'>
             <ProjectCard title="Resturant Website"
-            main="this is a rasturent website create in bootstrap and use some features and using basic css and html" onClick={handleClick1}/>
+            main="this is a rasturent website create in bootstrap and use some features and using basic css and html" onDemoClick={handleDemo1} onSourceClick={handleSource1}
+            />
             <ProjectCard title="Currency Converter"
-            main="this is a Currency Convertor create in js and use some features and using  css and html" onClick={handleClick2}/>
+            main="this is a Currency Convertor create in js and use some features and using  css and html" onDemoClick={handleDemo2} onSourceClick={handleSource2} />
             <ProjectCard title="AI Assistent"
-            main="this is a AI Assistent create in js and use some features and using  css and html"onClick={handleClick3}/>
+            main="this is a AI Assistent create in js and use some features and using  css and html" onDemoClick={handleDemo3} onSourceClick={handleSource3} />
 
 
 
