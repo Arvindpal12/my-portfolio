e7ddd399a3f0bf60f6e170d5d3639779a5ea12a4
Arvind <palshahab1290@gmail.com>
2026-04-11 20:36:57 +0530

    chnage a images

--- src/components/Home-compo/Home.jsx
+++ src/components/Home-compo/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import studentA from "../../assets/studentA.png";
+// import studentA from "../../assets/image-arvind.png";
+import arvind from "../../assets/arvind.jpg";
 import TextChange from "../TextChange";
 import { motion } from "framer-motion";
 
@@ -18,7 +19,7 @@ const Home = () => {
     //       user-friendly websites that help businesses grow and stand out online.
     //     </p>
       <motion.div
-      className="text-white flex flex-wrap w-full h-auto justify-between item-start p-10 md:p-20"
+      className="text-white flex flex-wrap w-full h-auto justify-between items-start p-10 md:p-20"
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 2 }}
@@ -51,15 +52,22 @@ const Home = () => {
         <motion.button
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.95 }}
-          className="mt-5 md:md-10 text-white py-2 px-3 text-sm md:text-lg md:px-4 hover:opacity-85 duration-300 font-semibold rounded-3xl bg-[#465697]"
+          className="mt-6 md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:px-4 hover:opacity-85 duration-300 font-semibold rounded-3xl bg-[#465697]"
         >
           Contact Me
         </motion.button>
         </a>
       </motion.div>
-      <div className="flex flex-wrap item-center justify-around ">
-        <img className="w-full h-auto animate-updown " src={studentA} alt="" />
-      </div>
+      {/* RIGHT SECTION */}
+      <motion.div
+        className="md:w-2/4 flex justify-center items-center"
+        initial={{ opacity: 0, x: 100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ once: true }}
+      >
+        <img className="w-48 h-48 md:w-64 md:h-64 lg:w-80 lg:h-80 rounded-full object-cover animate-updown" src={arvind} alt="Arvind" />
+      </motion.div>
     </motion.div>
   );
 };
